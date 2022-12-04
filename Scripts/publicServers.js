3525c2232a4856d9bb2c04eec06d89c8f5e17b68
NTM Studio <microplayinteractive@gmail.com>
2022-12-04 11:42:07 +0100

    a

--- Scripts/publicServers.js
+++ Scripts/publicServers.js
@@ -1,8 +1,6 @@
-function loadPublicServers(nickname, users) {
+function addPublicServer(nickname, users) {
     const serversTable = document.getElementById("serversTable");
     const tr = document.createElement("tr");
     tr.innerHTML = `<td>${nickname}</td><td><span class="material-symbols-outlined" style="font-size: 25px; float: left;">account_circle</span>${users}/2</td><td><button join>Dołącz</button></td>`
     serversTable.appendChild(tr);
 }
-loadPublicServers("test");
-
